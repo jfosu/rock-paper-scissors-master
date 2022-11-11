8de56b7cf9e472e86fa95577c5ccf4669885f066
fosujohn <fosujohn02@gmail.com>
2022-11-11 08:16:46 +0000

    Desktop Rules Modal

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const buttons = document.querySelectorAll('.pick');
 const scoreEl = document.getElementById('score');
 const main = document.getElementById('main');
 const selection = document.getElementById('selection');
-const reset = document.getElementById('reset')
+const reset = document.getElementById('reset');
+const user_select = document.getElementById('user_select');
+const computer_select = document.getElementById('computer_select');
+const winner = document.getElementById('winner')
+
+const openBtn = document.getElementById('open');
+const closeBtn = document.getElementById('close');
+const modal = document.getElementById('modal');
+
 
 const choices = ['paper', 'rock', 'scissors'];
 
@@ -10,7 +18,6 @@ let score = 0;
 let userChoice = undefined;
 
 buttons.forEach((button) => {
-    button.id
     button.addEventListener('click', () => {
         userChoice = button.getAttribute('data-choice');
         
@@ -19,21 +26,38 @@ buttons.forEach((button) => {
 })
 
 reset.addEventListener('click', () => {
+    //SHOW THE MAIN | HIDE THE SELECTION
     main.style.display = 'flex';
     selection.style.display = 'none'; 
+})
+
+openBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
 })
 
 function checkWinner() {
     const computerChoice = pickRandomChoice();
 
+    updateSelection(user_select, userChoice);
+    updateSelection(computer_select, computerChoice);
+
     if (userChoice === computerChoice) {
         //DRAW
+        winner.innerText = 'draw';
     } else if (userChoice === 'paper' && computerChoice === 'rock' || userChoice === 'rock' && computerChoice === 'scissors' || userChoice === 'scissors' && computerChoice === 'paper') {
+        //USER WIN
         updateScore(1);
+        winner.innerText = 'win';
     } else {
+        //USER LOST
         updateScore(-1);
+        winner.innerText = 'lost';
     }
-
+    //SHOW THE SELECTION | HIDE THE MAIN
     main.style.display = 'none';
     selection.style.display = 'flex';
 }
@@ -50,3 +74,15 @@ function pickRandomChoice() {
     ;
 }
 
+function updateSelection(selectionEl, choice) {
+    //CLASS RESET
+    selectionEl.classList.remove('btn-paper');
+    selectionEl.classList.remove('btn-rock');
+    selectionEl.classList.remove('btn-scissors');
+
+    //UPDATE SELECTION & ICON
+    const img = selectionEl.querySelector('img');
+    selectionEl.classList.add(`btn-${choice}`);
+    img.src = `./images/icon-${choice}.svg`;
+    img.alt = choice;
+}
